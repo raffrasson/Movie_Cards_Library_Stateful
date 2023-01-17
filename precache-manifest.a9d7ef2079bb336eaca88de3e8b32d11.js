3e37f1fc83f335eacdefe5dc083d226ccc1813d1
raffrasson <raf.frasson@gmail.com>
2023-01-17 00:21:48 -0300

    Updates

---
+++ precache-manifest.a9d7ef2079bb336eaca88de3e8b32d11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14780fbc5c6adc0f6a2eb0d02066a3d7",
+    "url": "/Movie_Cards_Library/index.html"
+  },
+  {
+    "revision": "21d69e424a6fdb86d99b",
+    "url": "/Movie_Cards_Library/static/css/main.5d58c8a2.chunk.css"
+  },
+  {
+    "revision": "cbb6d5c7ef3b1ab528f2",
+    "url": "/Movie_Cards_Library/static/js/2.9de1bedc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Movie_Cards_Library/static/js/2.9de1bedc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21d69e424a6fdb86d99b",
+    "url": "/Movie_Cards_Library/static/js/main.e2a8e5d2.chunk.js"
+  },
+  {
+    "revision": "a8a48fe8802126360384",
+    "url": "/Movie_Cards_Library/static/js/runtime-main.fd6213c2.js"
+  }
+]);
